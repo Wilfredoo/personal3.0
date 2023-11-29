f05671cde2d44650505c6f8da38326778695c615
Wilfredo <wilfredo@Wilfredos-MacBook-Air.local>
2023-11-29 14:51:36 +0100

    added cv

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ReactGA from "react-ga4";
 import Home from './components/Home';
-import Projects from './components/Projects';
-import FoundYourSticker from './components/FoundYourSticker/FoundYourSticker';
+import { Document, Page } from "react-pdf";
+import Curriculum from './components/Curriculum';
 
 const TRACKING_ID = "G-65B2W1RSZ2";
 
 ReactGA.initialize(TRACKING_ID);
 ReactGA.send(document.location.pathname);
-    // ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search });
 
 function App() {
   return (
     <Router>
-    <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/foundyoursticker" element={<FoundYourSticker />} />
+        <Route path="/cv" element={<Curriculum />} />
       </Routes>
-      </Router>
+    </Router>
   );
 }
 
---
+++ src/components/Curriculum.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+// Keyframes for the moving arrow
+const move = keyframes`
+  0% {
+    transform: translateY(-5px);
+  }
+  50% {
+    transform: translateY(0px);
+  }
+  100% {
+    transform: translateY(-5px);
+  }
+`;
+
+// Keyframes for the glowing button
+const glow = keyframes`
+  0% {
+    box-shadow: 0 0 5px #0ff, 0 0 10px #0ff, 0 0 15px #0ff, 0 0 20px #0ff;
+  }
+  50% {
+    box-shadow: 0 0 10px #0ff, 0 0 20px #0ff, 0 0 25px #0ff, 0 0 30px #0ff;
+  }
+  100% {
+    box-shadow: 0 0 5px #0ff, 0 0 10px #0ff, 0 0 15px #0ff, 0 0 20px #0ff;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding: 20px;
+  text-align: center;
+  box-sizing: border-box;
+  background-color: #fafafa;
+  font-family: 'Arial', sans-serif;
+`;
+
+const Arrow = styled.div`
+  color: #333; // Normal text color, no glow
+  animation: ${move} 1.5s ease-in-out infinite;
+  font-size: 2rem;
+  margin-bottom: 20px;
+`;
+
+const Button = styled.button`
+  padding: 12px 25px;
+  border: none;
+  border-radius: 5px;
+  background-color: #333;
+  color: #fff;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  animation: ${glow} 1.5s ease-in-out infinite;
+
+  &:hover {
+    background-color: #555;
+  }
+`;
+
+const WelcomeText = styled.h1`
+  font-size: 2.5rem;
+  color: #333;
+  margin-bottom: 20px;
+`;
+
+const Curriculum = () => {
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = `${process.env.PUBLIC_URL}/Résumé_Wilfredo.pdf`;
+    link.download = 'CV Wilfredo.pdf';
+    link.style.display = 'none';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <Wrapper>
+      <WelcomeText>Fancy a peek at my CV? </WelcomeText>
+      <p>Grab it right here.</p>
+      <Arrow>↓</Arrow>
+      <Button onClick={handleDownload}>Download Résumé</Button>
+    </Wrapper>
+  );
+};
+
+export default Curriculum;
