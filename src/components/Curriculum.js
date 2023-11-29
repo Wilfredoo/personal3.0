40fe7963b4bbfb825b4fef387e9d0be597b9473e
Wilfredo <wilfredo@Wilfredos-MacBook-Air.local>
2023-11-29 15:05:52 +0100

    fix file naming

--- src/components/Curriculum.js
+++ src/components/Curriculum.js
@@ -72,8 +72,8 @@ const WelcomeText = styled.h1`
 const Curriculum = () => {
   const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = `${process.env.PUBLIC_URL}/Résumé_Wilfredo.pdf`;
-    link.download = 'CV Wilfredo.pdf';
+    link.href = `${process.env.PUBLIC_URL}/Resume_Wilfredo.pdf`;
+    link.download = 'Resume_Wilfredo.pdf';
     link.style.display = 'none';
     document.body.appendChild(link);
     link.click();
