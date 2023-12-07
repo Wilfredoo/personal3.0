0282cb63b706e54798a3e7d2ce771fe5ed17ee68
Wilfredo <wilfredo@Wilfredos-MacBook-Air.local>
2023-12-07 16:00:02 +0100

    added new video route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ReactGA from "react-ga4";
 import Home from './components/Home';
 import Curriculum from './components/Curriculum';
+import VideoWorkRedirect from './components/VideoWorkRedirect';
 
 const TRACKING_ID = "G-65B2W1RSZ2";
 
@@ -15,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cv" element={<Curriculum />} />
+        <Route path="/videowork" element={<VideoWorkRedirect />} />
       </Routes>
     </Router>
   );
---
+++ src/components/VideoWorkRedirect.js
@@ -0,0 +1,17 @@
+// VideoWorkRedirect.js
+import React from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const VideoWorkRedirect = () => {
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    // Replace with your YouTube channel URL
+    window.location.href = 'https://www.youtube.com/@wilcas5349/featured';
+  }, [navigate]);
+
+  return null; // Return null since this component doesn't render anything
+};
+
+export default VideoWorkRedirect;
