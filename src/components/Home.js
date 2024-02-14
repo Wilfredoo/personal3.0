9fb072bdfe5023fa366ad8c3d7a5e6773731a236
Wilfredo <cacao_de_verdad@protonmail.com>
2024-02-14 02:35:07 +0100

    improvements

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,150 +1,77 @@
 import React, { useState } from 'react';
 import picture1 from '../assets/images/picture_1.png';
 import picture2 from '../assets/images/picture_2.png';
-import styled, { keyframes } from 'styled-components';
-const shake = keyframes`
-  0% { transform: translate(1px, 1px) rotate(0deg); }
-  10% { transform: translate(-1px, -1px) rotate(-0.5deg); }
-  20% { transform: translate(-1px, 0px) rotate(0.5deg); }
-  30% { transform: translate(1px, 1px) rotate(0deg); }
-  40% { transform: translate(0px, -1px) rotate(0.5deg); }
-  50% { transform: translate(-1px, 1px) rotate(-0.5deg); }
-  60% { transform: translate(-1px, 0px) rotate(0deg); }
-  70% { transform: translate(1px, 0px) rotate(-0.5deg); }
-  80% { transform: translate(0px, -1px) rotate(0.5deg); }
-  90% { transform: translate(1px, 1px) rotate(0deg); }
-  100% { transform: translate(0px, -1px) rotate(-0.5deg); }
-`;
-const HomeContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  text-align: center; /* Center align text content */
-  font-family: 'Lunasima', sans-serif;
-  background-color: #FDF4F5; /* Change background color */
-
-`;
-
-const ImageContainer = styled.div`
-    width: 170px;
-    height: 170px;
-    align-self: center;
-    animation: ${shake} 0.5s;
-    animation-delay: 10s; // start animation after 5s delay
-    @media (min-width: 1000px) {
-        position:absolute;
-        width: 200px;
-        height: 200px;
-    }
-    `;
-
-const Image = styled.img`
-  width: 100%;
-  height: auto;
-  object-fit: cover;
-  border-radius: 50%;
-  animation: ${shake} 0.5s;
-  animation-delay: 20s; // start animation after 5s delay
-@media (min-width: 1000px) {
-    position:absolute;
-    left: -80px;
-}
-
- 
-`;
-
-const TitleContainer = styled.div`
-animation: ${shake} 0.5s;
-animation-delay: 30s; // start animation after 5s delay
-flex-grow: 1;
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-align-items: center;
-margin: 20px 0;
-`;
-
-const ParentContainer = styled.div`
-display: flex;
-position: absolute;
-
-@media (max-width: 1000px) {
-    left: -70px;
-}
-
-width: -webkit-fill-available;
+import arrow from '../assets/images/arrow.png';
 
 
-
-`;
-
-const Title = styled.p`
-  a {
-    color: black;  /* inherit color from parent */
-    text-decoration: none;  /* remove underline */
-  }
-  animation: ${shake} 0.5s;
-  animation-delay: 60s; // start animation after 5s delay
-
-`;
-
-const Message = styled.p`
-  position: fixed;
-  bottom: 20px;
-  color: white;
-  background: #2D4356;
-  padding: 10px;
-  border-radius: 1.5px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-  left: 50%; // Move it to the horizontal center
-  transform: translateX(-50%); // Center it horizontally
-`;
-
-
+import {
+    HomeContainer,
+    ImageContainer,
+    Image,
+    TitleContainer,
+    ParentContainer,
+    Title,
+    Message,
+    DetailContainer // Ensure this is defined in your styles.js
+} from './styles'; // Adjust the path as necessary
 
 const Home = () => {
-    // eslint-disable-next-line
     const [picture, setPicture] = useState(Math.random() < 0.5 ? picture1 : picture2);
-
+    const [currentView, setCurrentView] = useState(null);
     const [message, setMessage] = useState(null);
+    const [fadeDetail, setFadeDetail] = useState(false); // New state to control fade animations
 
+
+    
     const copyEmailToClipboard = (e) => {
         e.preventDefault();
-
-        // Replace 'your.email@example.com' with your actual email
         navigator.clipboard.writeText('inbox@wilfredocasas.com').then(() => {
             setMessage('Email copied to clipboard!');
+            setTimeout(() => setMessage(null), 3000);
+        }).catch((err) => console.error('Could not copy email: ', err));
+    };
 
-            // After 3 seconds, hide the message
-            setTimeout(() => {
-                setMessage(null);
-            }, 3000);
-        })
-            .catch((err) => {
-                // You can handle errors here
-                console.error('Could not copy email: ', err);
-            });
+    const handleViewChange = (view) => () => setCurrentView(view);
+
+    const renderDetailView = () => {
+        switch (currentView) {
+            case 'Software Developer':
+                return <p>Your detailed text about being a Software Developer...</p>;
+            case 'Filmmaker':
+                return <p>Your detailed text about being a Filmmaker...</p>;
+            case 'Entrepreneur':
+                return <p>Your detailed text about being an Entrepreneur...</p>;
+            default:
+                return null; // Or some default content
+        }
     };
 
     return (
         <HomeContainer>
-            <ParentContainer>
-                <ImageContainer>
-                    <Image src={picture} alt="Me" onClick={copyEmailToClipboard} />
-                </ImageContainer>
-                <TitleContainer>
-                    <Title><a href="https://github.com/Wilfredoo" target="_blank" rel="noopener noreferrer">Software Developer</a></Title>
-                    <Title><a href="https://www.youtube.com/@wilcas5349/featured" target="_blank" rel="noopener noreferrer">Filmmaker</a></Title>
-                    <Title><a href="https://www.jobdirecto.com/" target="_blank" rel="noopener noreferrer">Entrepreneur</a></Title>
-                </TitleContainer>
-            </ParentContainer>
+            {currentView ? (
+                <DetailContainer>
+                    {renderDetailView()}
+                    <ImageContainer>
 
+                    <Image src={arrow} alt="arrow" onClick={handleViewChange(null)} />
+                    </ImageContainer>
+
+                </DetailContainer>
+            ) : (
+                <ParentContainer>
+                    <ImageContainer>
+                        <Image src={picture} alt="Me" onClick={copyEmailToClipboard} />
+                    </ImageContainer>
+                    <TitleContainer>
+                        <Title onClick={handleViewChange('Software Developer')}>Software Developer</Title>
+                        <Title onClick={handleViewChange('Filmmaker')}>Filmmaker</Title>
+                        <Title onClick={handleViewChange('Entrepreneur')}>Entrepreneur</Title>
+                    </TitleContainer>
+                </ParentContainer>
+            )}
             {message && <Message>{message}</Message>}
-
         </HomeContainer>
     );
 };
-
 
 export default Home;
---
+++ src/components/styles.js
@@ -0,0 +1,112 @@
+import styled, { keyframes } from 'styled-components';
+
+export const shake = keyframes`
+  0% { transform: translate(1px, 1px) rotate(0deg); }
+  10% { transform: translate(-1px, -1px) rotate(-0.5deg); }
+  20% { transform: translate(-1px, 0px) rotate(0.5deg); }
+  30% { transform: translate(1px, 1px) rotate(0deg); }
+  40% { transform: translate(0px, -1px) rotate(0.5deg); }
+  50% { transform: translate(-1px, 1px) rotate(-0.5deg); }
+  60% { transform: translate(-1px, 0px) rotate(0deg); }
+  70% { transform: translate(1px, 0px) rotate(-0.5deg); }
+  80% { transform: translate(0px, -1px) rotate(0.5deg); }
+  90% { transform: translate(1px, 1px) rotate(0deg); }
+  100% { transform: translate(0px, -1px) rotate(-0.5deg); }
+`;
+export const HomeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center; /* Center align text content */
+  font-family: 'Lunasima', sans-serif;
+  background-color: #FDF4F5; /* Change background color */
+
+`;
+
+export const ImageContainer = styled.div`
+    width: 170px;
+    height: 170px;
+    align-self: center;
+    animation: ${shake} 0.5s;
+    animation-delay: 10s; // start animation after 5s delay
+    @media (min-width: 1000px) {
+        position:absolute;
+        width: 200px;
+        height: 200px;
+    }
+    `;
+
+export const Image = styled.img`
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+  border-radius: 50%;
+  animation: ${shake} 0.5s;
+  animation-delay: 20s; // start animation after 5s delay
+@media (min-width: 1000px) {
+    position:absolute;
+    left: -80px;
+}
+
+ 
+`;
+
+export const TitleContainer = styled.div`
+animation: ${shake} 0.5s;
+animation-delay: 30s; // start animation after 5s delay
+flex-grow: 1;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+align-items: center;
+margin: 20px 0;
+`;
+
+export const ParentContainer = styled.div`
+display: flex;
+position: absolute;
+transition: opacity 0.5s ease-in-out;
+
+@media (max-width: 1000px) {
+    left: -70px;
+}
+
+width: -webkit-fill-available;
+
+
+
+`;
+
+export const Title = styled.p`
+  a {
+    color: black;  /* inherit color from parent */
+    text-decoration: none;  /* remove underline */
+  }
+  animation: ${shake} 0.5s;
+  animation-delay: 60s; // start animation after 5s delay
+
+`;
+
+export const Message = styled.p`
+  position: fixed;
+  bottom: 20px;
+  color: white;
+  background: #2D4356;
+  padding: 10px;
+  border-radius: 1.5px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  left: 50%; // Move it to the horizontal center
+  transform: translateX(-50%); // Center it horizontally
+`;
+
+export const DetailContainer = styled.div`
+  animation: fadeIn 0.5s;
+  opacity: 1; // Start fully visible
+  transition: opacity 0.5s ease-in-out;
+
+  @keyframes fadeIn {
+    from { opacity: 0; }
+    to { opacity: 1; }
+  }
+`;
