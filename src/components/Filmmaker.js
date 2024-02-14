7e19e3c2d6d15bb0bf5a9de3396f5de62ef03984
Wilfredo <cacao_de_verdad@protonmail.com>
2024-02-14 17:07:18 +0100

    smallfix

--- src/components/Filmmaker.js
+++ src/components/Filmmaker.js
@@ -5,7 +5,7 @@ import youtubeIcon from '../assets/images/youtube.png'; // Make sure the path ma
 // Assuming YoutubeIcon is a styled component for displaying icons. If it's an image tag, you should use <img> instead.
 const Filmmaker = () => (
   <TextContainer>
-    <Text>After a massive layoff in 2022, I thought, why not make some short films?</Text>
+    <Text>After the tech industry layoffs in 2022, I thought: why not make some short films?</Text>
     <Text>This quickly turned into an absurd and ambitious goal: to make a short film a week and have 52 after a year.</Text>
     <Text>I fell short by less than half the target, but on the way I met great people, had fun and even got a selection for <a href="https://www.interfilm.de/interfilm-festival-2023/filmsuche/?tx_interfilmfestival_filmlist%5BeventItem%5D=2627&cHash=fc313cc06c5794abd605c18a74f5770f" target="_blank" rel="noopener noreferrer"> one of our shorts </a> at Interfilm 2023.</Text>
     <Text>Below is my youtube channel, which contains not only our films but also serves as an artsy audiovisual self-expression outlet.</Text>
