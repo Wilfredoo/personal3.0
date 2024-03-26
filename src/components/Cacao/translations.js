107b3223b5018eea8504e5448054fc8c28f6a8c6
Wilfredo <cacao_de_verdad@protonmail.com>
2024-03-26 01:55:55 +0100

    fi

---
+++ src/components/Cacao/translations.js
@@ -0,0 +1,93 @@
+const translations = {
+    en: {
+        welcomeText: "Welcome to my online chocolate shop.",
+        introText: "Choosing a chocolate bar usually involves a trade-off between health and pleasure. To eliminate this compromise, I make my bars with raw ingredients and minimal processing, allowing you to enjoy the best of both worlds.",
+        ingredientsProcesses: "What this chocolate has",
+        whatMakesUnique: "What this chocolate does not have",
+        processInfo: "Order a chocolate bar",
+        chocolateInfo: [
+            "Cacao: raw, organic, and fair trade from Peruvian farmers.",
+            "Cacao butter: also organic and fair trade, sourced from the same locations as the cacao.",
+            "Honey: raw and unfiltered, preserving all nutritional properties, and regionally sourced to ensure sustainability.",
+            "Sea salt: a pinch is added for flavor enhancement."
+        ],
+        uniqueChocolateText: [
+            "Choosing a chocolate bar often means deciding between health and pleasure. I wanted to eliminate this compromise, which is why I've created a bar using raw ingredients and minimal processing—allowing for the enjoyment of both worlds.",
+            "Contrastingly, other chocolate bars may undergo several processes:",
+            [
+                "Potassium carbonate is added during the 'Dutch process' to reduce cacao's natural bitterness and darken its color, but this can lead to the loss of up to 90% of the beneficial flavonoids.",
+                "Sugar, particularly refined sugar, is often criticized for its negative health impact.",
+                "Color additives, preservatives, and enhancers are commonly used to increase market appeal.",
+                "Cocoa powder is used in scenarios involving complex logistics or other processed forms of cacao."
+            ]
+        ],
+        orderInstructions: [
+            "Place your order below.",
+            "Once I have enough orders for a batch of 10 bars, you'll get a confirmation and payment instructions via WhatsApp.",
+            "After payment, I'll start making your chocolate bar and will ensure it gets to you promptly."
+        ],
+        orderForm: {
+            quantityLabel: "Quantity",
+            priceLabel: "Price (in Euros)",
+            priceAdjustmentText: "Adjust the price according to your budget, and we'll go from there.",
+            deliveryOptionLabel: "Delivery Option",
+            shipToAddressOption: "Ship to my address",
+            inPersonOption: "Give me my chocolate in person",
+            firstNamePlaceholder: "First Name",
+            lastNamePlaceholder: "Last Name",
+            addressPlaceholder: "Your shipping address",
+            extraInstructionsPlaceholder: "Any additional delivery instructions?",
+            submitOrderButton: "Submit Order",
+            submitOrderExplanation: "Clicking on 'Submit Order' will open WhatsApp and preformat a message that you can send me with your order details.",
+        },
+        languageButton: "Zu Deutsch wechseln",
+        footer: "Something not working as expected? Send me a message on Signal or WhatsApp to +4915781295360 with your complaints."
+    },
+    de: {
+        welcomeText: "Willkommen in meinem Online-Schokoladenshop.",
+        introText: "Die Wahl eines Schokoriegels bedeutet oft, sich zwischen Gesundheit und Genuss zu entscheiden. Um diesen Kompromiss zu vermeiden, stelle ich meine Riegel aus rohen Zutaten und mit minimaler Verarbeitung her, sodass Sie das Beste aus beiden Welten genießen können.",
+        ingredientsProcesses: "Was diese Schokolade enthält",
+        whatMakesUnique: "Was diese Schokolade nicht enthält",
+        howToGetBar: "Wie du deine Schokoladentafel bekommst",
+        processInfo: "Bestelle einen Schokoladenriegel",
+        chocolateInfo: [
+            "Kakao: roh, bio und fair gehandelt von peruanischen Bauern.",
+            "Kakaobutter: auch bio und fair gehandelt, bezogen aus denselben Gegenden wie der Kakao.",
+            "Honig: roh und ungefiltert, um alle Nährwerte zu erhalten, und regional bezogen, um Nachhaltigkeit zu gewährleisten.",
+            "Meersalz: Eine Prise zur Geschmacksverstärkung hinzugefügt."
+        ],
+        uniqueChocolateText: [
+            "Wenn du dich für eine Schokoladentafel entscheidest, steht oft die Wahl zwischen Gesundheit und Genuss an. Ich wollte diesen Kompromiss nicht eingehen, weshalb ich eine Tafel mit rohen Zutaten und minimaler Verarbeitung kreiert habe – so kannst du beides in vollen Zügen genießen.",
+            "Im Gegensatz dazu gehen andere Schokoladenriegel durch mehrere Prozesse:",
+            [
+                "Kaliumcarbonat wird beim 'Dutch Process' hinzugefügt, um die natürliche Bitterkeit des Kakaos zu mindern und seine Farbe zu vertiefen, was jedoch bis zu 90% der nützlichen Flavonoide kosten kann.",
+                "Zucker, insbesondere raffinierter Zucker, wird oft wegen seiner negativen gesundheitlichen Auswirkungen kritisiert.",
+                "Farbzusätze, Konservierungsmittel und Verstärker werden häufig genutzt, um die Marktattraktivität zu erhöhen.",
+                "Kakaopulver wird in Szenarien verwendet, die eine komplexe Logistik oder andere verarbeitete Kakaoformen benötigen."
+            ]
+        ],
+        orderInstructions: [
+            "Bestell unten.",
+            "Sobald ich genug Bestellungen für 10 Riegel zusammenhabe, kriegst du eine Bestätigung und die Zahlungsinformationen per WhatsApp.",
+            "Nach der Zahlung leg ich los mit deinem Schokoriegel und sorg dafür, dass er schnell bei dir ist."
+        ],
+        orderForm: {
+            quantityLabel: "Menge",
+            priceLabel: "Preis (in Euro)",
+            priceAdjustmentText: "Pass den Preis nach deinem Budget an, und dann schauen wir weiter.",
+            deliveryOptionLabel: "Lieferoption",
+            shipToAddressOption: "An meine Adresse schicken",
+            inPersonOption: "Ich möchte meine Schokolade persönlich abholen",
+            firstNamePlaceholder: "Vorname",
+            lastNamePlaceholder: "Nachname",
+            addressPlaceholder: "Deine Versandadresse",
+            extraInstructionsPlaceholder: "Zusätzliche Lieferanweisungen?",
+            submitOrderButton: "Bestellung aufgeben",
+            submitOrderExplanation: "Wenn du auf 'Bestellung aufgeben' klickst, öffnet sich WhatsApp und du kannst mir eine Nachricht mit deinen Bestelldetails schicken.",
+        },
+        languageButton: "Zu Englisch wechseln",
+        footer: "Läuft etwas nicht wie erwartet? Schick mir eine Nachricht über Signal oder WhatsApp an +4915781295360 mit deinen Beschwerden."
+    }
+};
+
+export default translations;
