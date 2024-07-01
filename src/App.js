fe3492eb29b5e4e4b6e1f452895af9ec0714f83d
Wilfredo <cacao_de_verdad@protonmail.com>
2024-07-01 12:42:01 +0200

    updated links

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import ReactGA from "react-ga4";
 import Home from './components/Home';
 import Curriculum from './components/Curriculum';
 import VideoWorkRedirect from './components/VideoWorkRedirect';
-import BadVideoWorkRedirect from './components/BadVideoWorkRedirect';
 import BerlinBreakIn from './components/BerlinBreakIn';
 import Cacao from './components/Cacao/Cacao';
 import BatchOrders from './components/Cacao/BatchOrders';
+import ScriptsRedirect from './components/ScriptsRedirect';
 
 const TRACKING_ID = "G-65B2W1RSZ2";
 
@@ -22,7 +22,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/cv" element={<Curriculum />} />
         <Route path="/film" element={<VideoWorkRedirect />} />
-        <Route path="/badfilm" element={<BadVideoWorkRedirect />} />
+        <Route path="/scripts" element={<ScriptsRedirect />} />
         <Route path="/berlin-break-in" element={<BerlinBreakIn />} />
         <Route path="/cacao" element={<Cacao />} />
         <Route path="/kakao" element={<Cacao />} />
