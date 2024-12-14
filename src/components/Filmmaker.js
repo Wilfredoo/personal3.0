52ce761b9063c57067a5c954f37062ea46b3d6af
Wilfredo <cacao_de_verdad@protonmail.com>
2024-12-14 13:10:50 +0100

    update

--- src/components/Filmmaker.js
+++ src/components/Filmmaker.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Text, TextContainer, YoutubeIcon } from './styles';
-import youtubeIcon from '../assets/images/youtube.png'; // Make sure the path matches your file structure
+import youtubeIcon from '../assets/images/youtube.png'; // Ensure the path is correct
 
-// Assuming YoutubeIcon is a styled component for displaying icons. If it's an image tag, you should use <img> instead.
 const Filmmaker = () => (
   <TextContainer>
-    <Text>After the tech industry layoffs in 2022 and with lots of free time on my hands, I thought: why not make some short films?</Text>
-    <Text>This quickly turned into a challenge that was as absurd as it was ambitious: to make a short film a week until I reached 52 after a year.</Text>
-    <Text>I fell short by less than half, but along the way, I met great people and even got <a href="https://www.interfilm.de/interfilm-festival-2023/filmsuche/?tx_interfilmfestival_filmlist%5BeventItem%5D=2627&cHash=fc313cc06c5794abd605c18a74f5770f" target="_blank" rel="noopener noreferrer"> one of our shorts </a> selected at Interfilm 2023.</Text>
-    <Text>Below is my YouTube channel, which contains not only our films but also serves as an artsy audiovisual outlet for self-expression.</Text>
-    <Text>Oh, and more films are in the making.</Text>
-    <a href="https://www.youtube.com/channel/UC49b2y2ABFThhLKsmlC6KAw" target="_blank" rel="noopener noreferrer"><YoutubeIcon src={youtubeIcon} alt="YouTube" /></a>
+    <Text>One day back in 2022, I thought: why not make some short films?</Text>
+    <Text>
+      This tiny idea turned into an absurdly ambitious challenge—to make one a week until I had reached 52 films after a year.
+    </Text>
+    <Text>
+    I fell short by less than half, but the result was still pretty good; festival selections and meeting great folks along the way.
+    </Text>
+    <Text>
+    More, longer, better films are in sight.
+</Text>
+    <a
+      href="https://www.youtube.com/channel/UC49b2y2ABFThhLKsmlC6KAw"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <YoutubeIcon src={youtubeIcon} alt="YouTube" />
+    </a>
   </TextContainer>
 );
 
