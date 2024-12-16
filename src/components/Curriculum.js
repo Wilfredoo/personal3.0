5776364418ca06f805b937a0531ba72d2138364e
Wilfredo <cacao_de_verdad@protonmail.com>
2024-12-16 10:57:48 +0100

    update

--- src/components/Curriculum.js
+++ src/components/Curriculum.js
@@ -117,18 +117,17 @@ const LanguageButton = styled.button`
 
 const Curriculum = () => {
   const [language, setLanguage] = useState('EN');
-
+  
   const handleDownload = (resumeType) => {
-    const languageSuffix = language === 'EN' ? 'English' : 'German';
-    const fileName = `Wilfredo_${resumeType}_${languageSuffix}.pdf`;
     const link = document.createElement('a');
-    link.href = `${process.env.PUBLIC_URL}/${fileName}`;
-    link.download = fileName;
+    link.href = `${process.env.PUBLIC_URL}/Resume_${resumeType}_${language === 'EN' ? 'English' : 'German'}.pdf`;
+    link.download = `Wilfredo_${resumeType}_${language === 'EN' ? 'English' : 'German'}.pdf`;
     link.style.display = 'none';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
 };
+
 
 
   const toggleLanguage = () => {
