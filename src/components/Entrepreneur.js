0f70f53b906c7b3106491e0a1be197b257b854fe
Wilfredo <cacao_de_verdad@protonmail.com>
2025-05-31 12:08:23 +0200

    some changes in copy

--- src/components/Entrepreneur.js
+++ src/components/Entrepreneur.js
@@ -5,15 +5,16 @@ import linkedinIcon from '../assets/images/linkedin.png'; // Ensure the path is 
 const Entrepreneur = () => (
   <TextContainer>
     <Text>
-      For over a decade, I’ve started a dozen ideas across three different cities. Most did not work, but some did.
+      For over a decade, I’ve started a dozen ideas at least. Most did not work, but some did.
     </Text>
     <Text>
-  Successes include <a href="https://www.jobdirecto.com" target="_blank" rel="noopener noreferrer">Jobdirecto</a>, connecting thousands of Hispanic immigrants with jobs in New York, and <a href="https://boardgamegeek.com/boardgame/342455/bamberg" target="_blank" rel="noopener noreferrer">Bamberg</a>, a board game with global distribution, now evolving into <a href="https://www.instagram.com/play_ayakucho/" target="_blank" rel="noopener noreferrer">Ayakucho</a>.
+  One of them is <a href="https://www.jobdirecto.com" target="_blank" rel="noopener noreferrer">Jobdirecto</a>, a platform connecting thousands of Hispanic immigrants with jobs in New York.
+</Text>
+    <Text>
+  
+  Another is <a href="https://boardgamegeek.com/boardgame/342455/bamberg" target="_blank" rel="noopener noreferrer">Bamberg</a>, a board game with global distribution, now evolving into <a href="https://www.instagram.com/play_ayakucho/" target="_blank" rel="noopener noreferrer">Ayakucho</a>.
 </Text>
 
-<Text>
-  I now apply this experience at <a href="https://www.heidi-hire.com/en/home" target="_blank" rel="noopener noreferrer">heidi</a>, using my unorthodox thinking and guerrilla-style approach to lead business development and operations in South America.
-</Text>
 
 <a
       href="https://www.linkedin.com/in/wilfredocasas/"
--- src/components/SoftwareDeveloper.js
+++ src/components/SoftwareDeveloper.js
@@ -9,7 +9,8 @@ const SoftwareDeveloper = () => (
   <TextContainer>
 <Text>In recent years, I built numerous software apps using technologies like React and Node.js.</Text>
 <Text>Later, I spent 2 years at <a href="https://www.endava.com/" target="_blank" rel="noopener noreferrer">Endava</a>, contributing to various projects, most notably <a href="https://www.ardplus.de/" target="_blank" rel="noopener noreferrer">ARD+ for Smart TVs</a>.</Text>
-    <Text>Currently, I help with the development of our site <a href="https://www.jobdirecto.com" target="_blank" rel="noopener noreferrer">Jobdirecto</a>, where my best friend, <a href="https://jneidel.com/" target="_blank" rel="noopener noreferrer">Jonathan</a>, serves as the CTO.</Text>
+    <Text>Currently, I maintain and continue building <a href="https://www.jobdirecto.com" target="_blank" rel="noopener noreferrer">Jobdirecto</a>, focusing on always improving the users' experience.</Text>
+
     <a href="https://github.com/Wilfredoo" target="_blank" rel="noopener noreferrer"> <GithubIcon src={github} alt="github" /></a>
 
   </TextContainer>
