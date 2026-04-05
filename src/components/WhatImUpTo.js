788e77b8225a4a1a6fbb09d862b48cd3f1b99f8f
Wilfredo <cacao_de_verdad@protonmail.com>
2026-04-05 23:57:13 +0200

    fix

--- src/components/WhatImUpTo.js
+++ src/components/WhatImUpTo.js
@@ -4,7 +4,7 @@ import { Text, TextContainer } from './styles';
 const WhatImUpTo = () => (
   <TextContainer style={{ marginTop: 'clamp(20px, 10vh, 120px)' }}>
 <Text>
-  Currently, I create <a href="/apps"> apps</a> and <a href="/board-games">board games</a>,{' '}pursue
+  Currently, I create <a href="/apps"> apps</a> and <a href="/board-games">board games</a>,{' '}pursue{' '}
   <a href="https://www.instagram.com/wilfredocasas.actor/" target="_blank" rel="noopener noreferrer">acting</a>,{' '}
   and work on my first feature film, <a href="/healingisforgetting">Healing Is Forgetting</a>.
 </Text>
